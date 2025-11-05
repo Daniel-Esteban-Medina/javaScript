cba67a8b7e52bcd348b91b06b19b2eb1023512d5
diurno <diurno@B27-B3.lan.tierno.es>
2025-11-05 13:09:15 +0100

    ejercicio 6 terminado y 7 empezado

--- EjerciciosFunci6/ejercicio6.js
+++ EjerciciosFunci6/ejercicio6.js
@@ -1,1 +1,18 @@
 // 6.Función que reciba tres parámetros (a,b,c) y devuelva un array con las soluciones de una ecuación de segundo grado. (-b+-sqrt(b**2-4*a*c)/2*a)
+function ecuSegundoGrado(a, b, c){
+    let arr = [];
+    let raiz = (b ** 2) - 4 * a * c;
+    if(raiz < 0){
+        arr[0] = "No hay soluciones reales.";
+        arr[1] = "No hay soluciones reales.";
+    } else {
+        raiz = Math.sqrt(raiz);
+        // Math.sqrt(x) devuelve la raiz cuadrada
+        let dividendo = 2 * a;
+        arr[0] = (-b + raiz) / dividendo;
+        arr[1] = (-b - raiz) / dividendo;
+    }
+    return arr;
+}
+let array = ecuSegundoGrado(1, 5, 6);
+alert("1º solución: "+array[0]+"\n2º solución: "+array[1]);
---
+++ EjerciciosFunci6/ejercicio7.js
@@ -0,0 +1,1 @@
+// 7.Crea una función que reciba un número y un callback. Si el número es mayor a 10, ejecuta el callback.
