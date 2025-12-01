914756eb17abf4a5e84a32f76cb52f0dd5b350c3
diurno <diurno@B27-B3.lan.tierno.es>
2025-12-01 08:45:15 +0100

    5

--- EjerciciosMasMetodos8/ejercicio5.js
+++ EjerciciosMasMetodos8/ejercicio5.js
@@ -3,8 +3,10 @@ class Punto{
         this.valX = valX;
         this.valY = valY;
     }
-    static distancia(punto1, punto2){   // SIN TERMINAR
-
+    static distancia(punto1, punto2){
+        let px = (punto1.valX > punto2.valX ? (punto1.valX - punto2.valX) : (punto2.valX - punto1.valX));
+        let py = (punto1.valY > punto2.valY ? (punto1.valY - punto2.valY) : (punto2.valY - punto1.valY));
+        let distancia = Math.sqrt((px * px) + (py * py));
     }
 }
 alert(distancia(new Punto(15,27), new Punto(43,7)));
