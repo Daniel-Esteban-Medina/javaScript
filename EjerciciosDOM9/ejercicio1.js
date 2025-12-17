e0dde2f8c50dcf440542ed78a0cdfcd9449e77eb
diurno <diurno@B27-B3.lan.tierno.es>
2025-12-17 13:12:54 +0100

    ejercicios DOM 9 empezados

--- EjerciciosDOM9/ejercicio1.js
+++ EjerciciosDOM9/ejercicio1.js
@@ -1,3 +1,5 @@
+// 1- Crea un html con varias etiquetas de muestra. La página pedirá que se introduzca una etiqueta y 
+// saldrá por consola todo el contenido de las etiquetas señaladas.
 const respuesta = prompt("introduce una etiqueta.");
 const etiqueta = document.getElementsByTagName(respuesta);
 for (let item of etiqueta) {
---
+++ EjerciciosDOM9/ejercicio2.js
@@ -0,0 +1,1 @@
+// 2- Añade a una página un h1 que no existiera en HTML con un título.
