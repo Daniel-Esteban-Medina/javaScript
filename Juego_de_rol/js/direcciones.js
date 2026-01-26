8fca13be945a43c6dc4e168c7bd191bf5c553271
diurno <diurno@B27-B3.lan.tierno.es>
2026-01-26 12:37:36 +0100

    juego rol v2

--- Juego_de_rol/js/direcciones.js
+++ Juego_de_rol/js/direcciones.js
@@ -1,12 +1,35 @@
-function irInicio(){
-    window.location.href = "./inicio.html";
+let aventurero = {
+    xp: 0,
+    salud: 100,
+    dinero: 50,
+    atk: 6
+};
+localStorage.setItem("objAventur", aventurero);
+
+const botTienda = document.getElementById("botTienda");
+if(botTienda != null){
+    botTienda.addEventListener("click", function(){
+        window.location.href = "./tienda.html";
+    });
 }
-function irTienda(){
-    window.location.href = "./tienda.html";
+
+const botInicio = document.getElementById("botInicio");
+if(botInicio != null){
+    botInicio.addEventListener("click", function(){
+        window.location.href = "./inicio.html";
+    });
 }
-function irEntrenar(){
-    window.location.href = "./entrenar.html";
+
+const botEntrenar = document.getElementById("botEntrenar");
+if(botEntrenar != null){
+    botEntrenar.addEventListener("click", function(){
+        window.location.href = "./entrenar.html";
+    });
 }
-function irDragon(){
+
+const botDragon = document.getElementById("botDragon");
+if(botDragon != null){
+botDragon.addEventListener("click", function(){
     window.location.href = "./dragon.html";
+});
 }
