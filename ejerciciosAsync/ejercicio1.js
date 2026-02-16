ea4d8536153c7ab791a32a4116a48db1d94f97d9
diurno <diurno@B27-B3.lan.tierno.es>
2026-02-16 12:14:28 +0100

    ejercicios async, creados

---
+++ ejerciciosAsync/ejercicio1.js
@@ -0,0 +1,3 @@
+//Crear una función llamada hacerTarea que devuelva una promesa, espere un segundo y se resuelva con el mensaje “Tarea completada”. 
+//Crea una función run() marcada como async que llame a hacerTarea usando await e imprima por consola el mensaje que devuelve.
+
---
+++ ejerciciosAsync/ejercicio2.js
@@ -0,0 +1,5 @@
+//Crea una función llamada doblar que reciba un número como parámetro, devuelva una promesa y espere un segundo, 
+//resolviendo con el número mutiplicado por dos si el número es positivo y rechazando con “Utiliza solo números positivos” si no. 
+//Crea una función run() marcada como async que llame a doblar(5) usando await, y vuelve a doblar el resultado volviendo a llamar 
+//a la función doblar para luego imprimir en consola “Resultado: X”
+
