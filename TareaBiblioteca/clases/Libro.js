645f518c395cc9a3ba6b3aab1823e5a39e2e1317
diurno <diurno@B27-B3.lan.tierno.es>
2026-02-19 14:28:28 +0100

    mas tareabiblio

--- TareaBiblioteca/clases/Libro.js
+++ TareaBiblioteca/clases/Libro.js
@@ -1,15 +1,18 @@
 export class Libro{
-    constructor(titulo, autor, numPaginas, genero, estaPrestado){
+    constructor(titulo, autor, numPaginas, genero, estaPrestado, fechaPrestamo = null){
         this.titulo = titulo;
         this.autor = autor;
         this.numPaginas = numPaginas;
         this.genero = genero;
         this.estaPrestado = estaPrestado;
+        this.fechaPrestamo = fechaPrestamo;
     }
     marcarPrestado(){
         this.estaPrestado = true;
+        this.fechaPrestamo = new Date();
     }
     marcarDevuelto(){
         this.estaPrestado = false;
+        this.echaPrestamo = null;
     }
 }
--- TareaBiblioteca/clases/Usuario.js
+++ TareaBiblioteca/clases/Usuario.js
@@ -1,13 +1,19 @@
 export class Usuario{
 
-    constructor(nombre, password, rol, librosPrestados){
+    constructor(nombre, password, rol, librosPrestados, estaPenalizado = false){
         this.nombre = nombre;
         this.password = password; 
         this.rol = rol;
         this.librosPrestados = librosPrestados;
+        this.estaPenalizado = estaPenalizado;
     }
     pedirLibro(lib){
-        this.librosPrestados.push(lib);
+        if(this.estaPenalizado == false && librosPrestados.legth <= 3){
+            this.librosPrestados.push(lib);
+            return "Prestamo concedido.";
+        } else {
+            return "Prestamo denegado.";
+        }
     }
     devolverLibro(tituloLibro){
         this.librosPrestados  = this.librosPrestados.filter(lib => lib.titulo != tituloLibro); 
@@ -16,5 +22,14 @@ export class Usuario{
     rellenarTabla(){
         let tablaUsuarios = document.getElementById("tablaUsuarios");
         
+    }
+    merecePenalizacion(){
+        librosPrestados.forEach(libro => {
+            let diferenciaMs = (new Date()) - libro.fechaPrestamo;
+            const dias = Math.floor( diferenciaMs / (1000 * 60 * 60 * 24));
+            if(dias > 14){
+                estaPenalizado = true;
+            }
+        });
     }
 }
