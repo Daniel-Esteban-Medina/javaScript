6b5aca44e585dd1561648afd5eca0695d844ff83
diurno <diurno@B27-B3.lan.tierno.es>
2026-02-20 14:28:52 +0100

    js avances

--- retoPokemon/ejercicio1.js
+++ retoPokemon/ejercicio1.js
@@ -1,8 +1,21 @@
+let boton = document.getElementById("boton");
 async function obtenerDatos(){
-    let respuesta = await fetch("https://pokeapi.co/api/v2");
-    let resultado = await respuesta.json();
-    const enlace = resultado["pokemon-species"];
-    console.log(enlace);
+    let nombre = document.getElementById("nombre").value;
+    let respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/"+nombre);
+    pokemon = await respuesta.json();
+    console.log(pokemon);
+    let name = pokemon.name;
+    let tipo1 = pokemon.types["0"].type.name;
+    let tipo2 = (pokemon.types["1"] == undefined) ? "" : pokemon.types["1"].type.name;
+
+    let imagen = pokemon.sprites.front_default;
+    let img = document.createElement("div");
+    img.innerHTML = "<img src='"+imagen+"'></img>";
+    document.body.appendChild(img);
+
+    console.log("nombre: "+name+((tipo2 == "") ? " tipo: "+tipo1 : " tipos: "+tipo1+"/"+tipo2));
 }
-let tabla = document.getElementById("tabla");
-obtenerDatos();
+
+boton.addEventListener("click", function(){
+    obtenerDatos();
+});
