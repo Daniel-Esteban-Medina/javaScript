c7aecc54bbb69b678a5ced3b47022e1f47dbad32
Daniel-Esteban-Medina <37danielesteban37@gmail.com>
2026-02-23 01:43:37 +0100

    ejercicios async terminados

--- ejerciciosAsync/ejercicio1.js
+++ ejerciciosAsync/ejercicio1.js
@@ -1,12 +1,17 @@
 //Crear una función llamada hacerTarea que devuelva una promesa, espere un segundo y se resuelva con el mensaje “Tarea completada”. 
 //Crea una función run() marcada como async que llame a hacerTarea usando await e imprima por consola el mensaje que devuelve.
 
-function hacerTarea(){
-    setTimeout(function(){
-
-    }, 1000);
+function hacerTarea() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Tarea completada");
+        }, 1000);
+    });
 }
 
-async function run(){
-
+async function run() {
+    let resultado = await hacerTarea();
+    console.log(resultado);
 }
+
+run();
--- ejerciciosAsync/ejercicio2.js
+++ ejerciciosAsync/ejercicio2.js
@@ -3,3 +3,26 @@
 //Crea una función run() marcada como async que llame a doblar(5) usando await, y vuelve a doblar el resultado volviendo a llamar 
 //a la función doblar para luego imprimir en consola “Resultado: X”
 
+function doblar(n) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (n >= 0) { 
+                resolve(n * 2);
+            } else { 
+                reject("Utiliza solo números positivos");
+            }
+        }, 1000);
+    });
+}
+
+async function run() {
+    try {
+        let num1 = await doblar(5);
+        let num2 = await doblar(num1);
+        console.log("Resultado: "+num2);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+run();
