d1db238d92a5615116e804237d335cf79a0d1a0a
diurno <diurno@B27-B3.lan.tierno.es>
2026-02-23 12:08:49 +0100

    retoPokemon audio implementado

--- retoPokemon/ejercicio1.js
+++ retoPokemon/ejercicio1.js
@@ -5,8 +5,9 @@ async function obtenerDatos(){
     pokemon = await respuesta.json();
     console.log(pokemon);
     let name = pokemon.name;
-    let tipo1 = pokemon.types["0"].type.name;
-    let tipo2 = (pokemon.types["1"] == undefined) ? "" : pokemon.types["1"].type.name;
+    let tipo1 = tr[pokemon.types["0"].type.name];
+    let tipo2 = (pokemon.types["1"] == undefined) ? "" : tr[pokemon.types["1"].type.name];
+    let sonido = new Audio(pokemon.cries.latest);
 
     let imagen = pokemon.sprites.front_default;
     let img = document.createElement("div");
@@ -14,8 +15,40 @@ async function obtenerDatos(){
     document.body.appendChild(img);
 
     console.log("nombre: "+name+((tipo2 == "") ? " tipo: "+tipo1 : " tipos: "+tipo1+"/"+tipo2));
+
+    crearBotonSonido(sonido);
 }
 
 boton.addEventListener("click", function(){
     obtenerDatos();
 });
+
+const tr = {
+    "electric" : "electrico",
+    "fire" : "fuego",
+    "flying" : "volador",
+    "water" : "agua",
+    "bug" : "bicho",
+    "grass" : "planta",
+    "ice" : "hielo",
+    "ground" : "tierra",
+    "rock" : "roca",
+    "steel" : "acero",
+    "poison" : "veneno",
+    "dark" : "siniestro",
+    "fighting" : "lucha",
+    "fairy" : "hada",
+    "psychic" : "psiquico",
+    "dragon" : "dragón",
+    "ghost" : "fantasma",
+    "normal" : "normal"
+};
+
+function crearBotonSonido(sonido){
+    let boton = document.createElement("button");
+    boton.textContent = "🔊";
+    boton.addEventListener("click", ()=>{
+        sonido.play();
+    });
+    document.body.appendChild(boton);
+}
